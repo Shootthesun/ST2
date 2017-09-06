e15c5234295b06ace04dc781e5e797d00434cdfc
sown5ot <sonnguyenhoang2310@gmail.com>
2017-09-06 20:54:12 +0700

    Fixed horizontalphysics

--- src/ST2/Player/Player.java
+++ src/ST2/Player/Player.java
@@ -48,15 +48,15 @@ public class Player extends GameObject implements PhysicsBody {
     public void run(Vector2D parentPosition) {
         super.run(parentPosition);
         updatePhysics();
-        this.position.addUp(velocity);
     }
+
     private void updatePhysics(){
         velocity.y += Gravity;
         velocity.x = 0;
         jump();
         updateVerticalPhysics();
         move();
-//        updateHorizontalPhysics();
+        updateHorizontalPhysics();
 
 //        if (contraints != null) {
 //            contraints.make(position);
@@ -108,58 +108,22 @@ public class Player extends GameObject implements PhysicsBody {
         this.position.y += velocity.y;
         this.screenPosition.y += velocity.y;
     }
-    //    @Override
-//    public void run(Vector2D parentPosition) {
-//        super.run(parentPosition);
-////        updatePhysics();
-//        position.addUp(velocity);
 
-//    }
-
-//    private void updatePhysics() {
-//        velocity.y += GRAVITY;
-//        velocity.x = 0;
-//        move();
-//        updateVerticalPhysics();
-//    }
-
-//    private void updateVerticalPhysics() {
-//        Vector2D checkPosition = screenPosition.add(0, velocity.x);
-//        Platform platform = Physics.collideWith(checkPosition, boxCollider.getWidth(), boxCollider.getHeight(), Platform.class);
-//        if (platform != null){
-//            while (Physics.collideWith(screenPosition.add(0, Math.signum(velocity.y)), boxCollider.getWidth(), boxCollider.getHeight(), Platform.class) == null){
-//                position.add(0, Math.signum(velocity.y));
-//                screenPosition.add(0, Math.signum(velocity.y));
-//            }
-//            velocity.y = 0;
-//        }
-//        this.position.y += velocity.y;
-//        this.screenPosition.y += velocity.y;
-//    }
-//
-//
     private void updateHorizontalPhysics() {
         Vector2D checkPosition = screenPosition.add(velocity.x, 0);
         Platform platform = Physics.collideWith(checkPosition, boxCollider.getWidth(), boxCollider.getHeight(), Platform.class);
         if (platform != null){
+            float dx = Math.signum(velocity.x);
+            while (Physics.collideWith(screenPosition.add(dx, 0), boxCollider.getWidth(), boxCollider.getHeight(), Platform.class) == null){
+                position.addUp(dx, 0);
+                screenPosition.addUp(dx, 0);
+            }
             velocity.x = 0;
         }
+        this.position.x += velocity.x;
+        this.screenPosition.x += velocity.x;
     }
-//
-//    private void move() {
-//        if (inputManager.upPressed) {
-//            if (Physics.collideWith(screenPosition.add(0, 1),
-//                    boxCollider.getWidth(),
-//                    boxCollider.getHeight(),
-//                    Platform.class) != null) {
-//                velocity.y -= SPEED;
-//            }
-//        }
-//        //constraints
-//        if (contraints != null) {
-//            contraints.make(position);
-//        }
-//    }
+
 
     @Override
     public BoxCollider getBoxCollider() {
